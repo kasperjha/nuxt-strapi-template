9e32c8c71179d4af328b691e61025c79cea7c5c3
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-07-24 11:44:22 +0200

    feat: add my eslint overrides

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,3 +2,15 @@ import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(antfu())
+  .override('antfu/vue/rules', {
+    rules: {
+      'vue/block-order': ['error', {
+        order: ['template', 'script', 'style'],
+      }],
+      'vue/max-attributes-per-line': ['error', {
+        singleline: {
+          max: 2,
+        },
+      }],
+    },
+  })
